db5d3b603b5fc149436349e9b102c381a2326d7d
James Collins <metacom.tunes@gmail.com>
2020-07-26 14:57:32 -0400

    Updates

---
+++ precache-manifest.bc2456c76a762b366ea18c7c3511dc17.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f34a34ede77ee714b69a35e0cc8123e1",
+    "url": "/mrjamescollins/jdisplay/index.html"
+  },
+  {
+    "revision": "6d17582bbdca37d962fd",
+    "url": "/mrjamescollins/jdisplay/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "9834b0e99aec980d9b63",
+    "url": "/mrjamescollins/jdisplay/static/js/2.bf44bd17.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/mrjamescollins/jdisplay/static/js/2.bf44bd17.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6d17582bbdca37d962fd",
+    "url": "/mrjamescollins/jdisplay/static/js/main.f2fd76a5.chunk.js"
+  },
+  {
+    "revision": "af26907c1457ea6f67e8",
+    "url": "/mrjamescollins/jdisplay/static/js/runtime-main.103d6deb.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/mrjamescollins/jdisplay/static/media/logo.5d5d9eef.svg"
+  }
+]);
